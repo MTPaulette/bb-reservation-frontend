6cb49dd7c1cc3e5af0f0f17aaa0b275d10f5bc8e
MTPaulette <mayoguepaulette@gmail.com>
2025-01-09 20:27:44 +0100

    nettoyage du code: suppression de page et component inutils

--- app/[locale]/(client)/ressources/[id]/not-found.tsx
+++ app/[locale]/(client)/ressources/[id]/not-found.tsx
@@ -2,13 +2,13 @@ import NotFound from '@/components/NotFound';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "404 Agency not found | BB-RESERVATION-SYST",
+  title: "404 Ressource not found | BB-RESERVATION-SYST",
   description:
-    "The Agency is not found ",
+    "The Ressource is not found ",
 };
 
 export default function NotFoundPage() {
   return (
-    <NotFound title="agency_not_found" />
+    <NotFound title="ressource_not_found" />
   );
 }
